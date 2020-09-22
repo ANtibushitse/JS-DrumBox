1e3749bb63fcec094bf232f0c02ac9b2f78b22ae
Arsene Ntibushitse <ntibushitse.arsene@gmail.com>
2020-09-22 04:49:39 +0200

    try to debug    transition end firing

--- script.js
+++ script.js
@@ -4,18 +4,19 @@ function playSound(e) {
   if (!audio) return;// empêche la fonction de tourner en boucle 
 
   key.classList.add('playing');//Permets d'ajouter fonction play à key .remove et à toggle
-  audio.currentTime = 0;
+  audio.currentTime = 0; //  rejoue le sample depuis le début
   audio.play();
 }
-console.log("transition");
-function removeTransition(e) {
-  if (e.propertyName !== 'transform') return;
-  e.target.classList.remove('playing');
 
-}
+function removeTransition(e) {
+  if (e.propertyName !== 'transorm') return;
+  console.log(this);
+
+  //e.target.classList.remove('playing');
+};
 //Hello there :P 
 const keys = Array.from(document.querySelectorAll('.key'));
 // consol.log(keys) returen arrau
-keys.forEach(key => console.log('hgjg'));//  is this feasable? 
+// keys.forEach(key => console.log('hgjg'));//  is this feasable? 
 keys.forEach(key => key.addEventListener('transitioncancel', removeTransition));  
 window.addEventListener('keydown', playSound);
